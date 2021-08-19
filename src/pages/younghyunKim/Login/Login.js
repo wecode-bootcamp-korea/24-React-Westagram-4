897a1ddebd560450eb831cabad3f93df49099c99
(rhfovk) <(rhfovk@gmail.com)>
2021-08-19 19:57:02 +0900

    ✨ Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현 - Login js 및 scss 파일 업데이트

--- src/pages/younghyunKim/Login/Login.js
+++ src/pages/younghyunKim/Login/Login.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
+import "./Login.scss";
 
 class LoginYoungHyun extends Component {
+  state = [{ id: "" }, { pw: "" }];
+
+  handelIdInput = e => {
+    const i = e.target.value;
+    this.setState({ id: i });
+    console.log(this.state.id);
+  };
+
+  handelPwInput = e => {
+    const i = e.target.value;
+    this.setState({ pw: i });
+    console.log(this.state.pw);
+  };
+
   render() {
-    return <div>안녕하세요?</div>;
+    return (
+      <div className="login">
+        <div className="background">
+          <h1>Westagram</h1>
+          <div className="boxes">
+            <input
+              type="text"
+              className="id"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={this.handelIdInput}
+            />
+            <input
+              type="password"
+              className="pw"
+              placeholder="비밀번호"
+              onChange={this.handelPwInput}
+            />
+            <button type="button" className="loginBtn" onClick={this.goToMain}>
+              로그인
+            </button>
+          </div>
+          <span>비밀번호를 잊으셨나요?</span>
+        </div>
+      </div>
+    );
   }
 }
 

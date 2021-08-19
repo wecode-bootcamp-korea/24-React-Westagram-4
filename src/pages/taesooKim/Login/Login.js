d631c944c4c864a5df1b85c38335eaf7081f6829
orosy <hanei7632@gmail.com>
2021-08-19 19:18:50 +0900

    ✨ Add: Main 페이지 Aside 컴포넌트 추가

--- src/pages/taesooKim/Login/Login.js
+++ src/pages/taesooKim/Login/Login.js
@@ -5,7 +5,7 @@ import "../../../styles/common.scss";
 
 class Login extends React.Component {
   goToMain = () => {
-    this.props.history.push("/main-soo");
+    this.props.history.push("/main");
   };
 
   render() {

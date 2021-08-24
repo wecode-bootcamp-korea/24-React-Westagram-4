eec4be81a66caf14fbf26cc0670d5d34aac23509
Casey <hjkdw9506@gmail.com>
2021-08-24 15:16:40 +0900

    ♻️ Refactor: 구조분해 할당 적용

--- src/pages/hyunjaeKim/Main/Replies/Reply.js
+++ src/pages/hyunjaeKim/Main/Replies/Reply.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 
 export default class Reply extends Component {
   render() {
+    const { userName, content } = this.props;
     return (
       <li className="reply">
         <div className="replyDetail">
           <strong>
             <a href="#" className="userID">
-              {this.props.userName}
+              {userName}
             </a>
           </strong>
           <a href="#" className="comment hashtag">
-            {this.props.content}
+            {content}
           </a>
         </div>
         <div className="icon heartBtn small_icon">

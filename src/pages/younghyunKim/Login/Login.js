ab1cde222a382dad96b0358e5e2df968f37bcb51
(rhfovk) <(rhfovk@gmail.com)>
2021-08-26 09:45:47 +0900

    ⚡️ Add: 서버 연결 - fetch를 통한 회원가입 및 로그인  서버 연결

--- src/pages/younghyunKim/Login/Login.js
+++ src/pages/younghyunKim/Login/Login.js
@@ -19,8 +19,42 @@ class LoginYoungHyun extends Component {
     });
   };
 
+  // goToMain = () => {
+  //   fetch("http://10.58.7.215:8000/users/signin", {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       // name: "강남11",
+  //       emasil: this.state.id,
+  //       password: this.state.pw,
+  //       // cell_number: "010-2344-2423",
+  //       // address: "서울시 강남구 서초동 124",
+  //       // birthday: "2020-03-02",
+  //       // sex: "여",
+  //     }),
+  //   })
+  //     .then(response => response.json())
+  //     .then(result => {
+  //       result.TOKEN
+  //         ? this.props.history.push("/main-hyun")
+  //         : alert("다시 입력해주세요!");
+  //     });
+  // };
+
   goToMain = () => {
-    this.props.history.push("/main-hyun");
+    fetch("http://10.58.2.174:8000/users/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        name: "강남11",
+        email: this.state.id,
+        password: this.state.pw,
+        cell_number: "010-2341-2423",
+        address: "서울시 강남구 서초동 124",
+        birthday: "2020-13-02",
+        sex: "",
+      }),
+    })
+      .then(response => response.json())
+      .then(result => console.log("결과: ", result));
   };
 
   render() {

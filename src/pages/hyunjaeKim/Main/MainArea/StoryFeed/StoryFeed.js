24133797e6e315809024b09c5c863fbf62a6bf8a
Casey <hjkdw9506@gmail.com>
2021-08-29 16:30:33 +0900

    🚧 Add : storyfeed 버튼 클릭 시 translate 작동 작업 중

--- src/pages/hyunjaeKim/Main/MainArea/StoryFeed/StoryFeed.js
+++ src/pages/hyunjaeKim/Main/MainArea/StoryFeed/StoryFeed.js
@@ -4,6 +4,7 @@ import Story from "./Story";
 export default class StoryFeed extends Component {
   state = {
     users: [],
+    clicked: false,
   };
 
   componentDidMount() {
@@ -13,17 +14,31 @@ export default class StoryFeed extends Component {
       .then(res => res.json())
       .then(users => this.setState(users));
   }
+  handleClick = () => {
+    this.setState({ ...this.state, clicked: true });
+  };
+
+  // handleReset = () => {
+  //   const { clicked } = this.state;
+  //   const sign = false;
+  //   if (clicked === true) {
+  //     this.setState(prev => ({ ...prev, clicked: sign }));
+  //   }
+  //   console.log(this.state);
+  // };
 
   render() {
-    const { users } = this.state;
+    const { users, clicked } = this.state;
     return (
       <section className="storyFeed">
-        <ul className="stories content-box">
-          {users.map((user, idx) => (
-            <Story user={user} key={idx} />
-          ))}
-        </ul>
-        <button className="showNext icon"></button>
+        <div className="content-box">
+          <ul className={`stories ${clicked ? "move" : ""}`}>
+            {users.map((user, idx) => (
+              <Story user={user} key={idx} />
+            ))}
+          </ul>
+        </div>
+        <button className="showNext icon" onClick={this.handleClick}></button>
       </section>
     );
   }

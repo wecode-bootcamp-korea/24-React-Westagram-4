dca483b115861581179c6b586b91bd5d9ac0057c
Casey <hjkdw9506@gmail.com>
2021-08-29 14:34:36 +0900

    ✨ Add: more버튼 클릭 시 본문 내용 펼쳐지도록 기능 추가

--- src/pages/hyunjaeKim/Main/MainArea/Contents/DetailWrapper.js
+++ src/pages/hyunjaeKim/Main/MainArea/Contents/DetailWrapper.js
@@ -5,6 +5,7 @@ export default class DetailWrapper extends Component {
   state = {
     likes: 1515,
     isRed: false,
+    isOpened: false,
   };
 
   handleDelete = reply => {
@@ -17,6 +18,10 @@ export default class DetailWrapper extends Component {
     });
   };
 
+  handleOpen = () => {
+    this.setState({ ...this.state, isOpened: true });
+  };
+
   handleIncrement = () => {
     const { isRed, likes } = this.state;
     const sign = { true: 1, false: -1 };
@@ -27,6 +32,7 @@ export default class DetailWrapper extends Component {
   };
 
   render() {
+    const { isOpened, likes } = this.state;
     return (
       <div className="detailWrapper content-box">
         <div className="contentDetail">
@@ -45,21 +51,28 @@ export default class DetailWrapper extends Component {
             <button type="button" className="icon bookmark"></button>
           </section>
           <div className="likes userID">
-            <strong className="likedNumber">{this.state.likes}</strong> likes
+            <strong className="likedNumber">{likes}</strong> likes
           </div>
-          <div className="writterComment">
-            <strong>
-              <a href="#" className="userID">
-                koung_bear
-              </a>
-            </strong>
-            <span className="comment">
-              동메달보다 금메달보다 어떤 메달과 상보다 더 값진 감동을 선물해줘서
-              너무너무 고마워요 덕분에 전국민이 국뽕 한 사발 들이키고 목 터져라
-              김연경 울부짖었던 행복했던 올림픽이었어요 절대절대 자책하지말고
-              푸욱 쉬어요💙
-              <span className="moreLink">more</span>
+          <div className="writterCommentContainer">
+            <span className={`writterCommentWrapper ${isOpened ? "open" : ""}`}>
+              <strong>
+                <a href="#" className="userID">
+                  koung_bear
+                </a>
+              </strong>
+              <span className="writterComment">
+                동메달보다 금메달보다 어떤 메달과 상보다 더 값진
+                감동을선물해줘서 너무너무 고마워요 덕분에 전국민이 국뽕 한 사발
+                들이키고 목 터져라 김연경 울부짖었던 행복했던 올림픽이었어요
+                절대절대 자책하지말고 푸욱 쉬어요💙
+              </span>
             </span>
+            <button
+              className={`moreLink ${isOpened ? "open" : ""}`}
+              onClick={this.handleOpen}
+            >
+              more
+            </button>
           </div>
           <div className="userComments">
             <a href="#" className="moreLink">
